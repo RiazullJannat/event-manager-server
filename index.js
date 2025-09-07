ae48164d641eb33cd8158b5bc78b3ebbdf642177
RJannat <ramimk097@gmail.com>
2025-09-07 12:04:03 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+// task-manager
+// F2mGgEBZwRpL7JIS
+
+const uri = "mongodb+srv://task-manager:F2mGgEBZwRpL7JIS@cluster0.e3b4z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res)=>{
+    res.send("simple crud is running");
+})
+app.listen(port, ()=>{
+    console.log(`Simple crud is running on port: ${port}`);
+})
